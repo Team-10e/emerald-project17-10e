297aa5d321a03dea2f95df42dfbb05906c718922
mpetty231157 <mpetty@ufl.edu>
2023-11-09 13:42:07 -0500

    add unit/add lesson
    ethan added lesson/unit creation

--- client/src/views/Mentor/Classroom/Classroom.jsx
+++ client/src/views/Mentor/Classroom/Classroom.jsx
@@ -1,12 +1,17 @@
-import {React, useEffect} from 'react';
+import {React, useEffect, useState} from 'react';
 import { Tabs } from 'antd';
 import './Classroom.less';
-
+import { Button, Form, Input, message, Modal } from "antd";
+import { getGrades, getLessonModuleAll } from '../../../Utils/requests';
 import NavBar from '../../../components/NavBar/NavBar';
 import Roster from './Roster/Roster';
 import Home from './Home/Home';
 import SavedWorkSpaceTab from '../../../components/Tabs/SavedWorkspaceTab';
 import { useSearchParams, useParams } from 'react-router-dom';
+import { createUnit } from "../../../Utils/requests";
+import LessonModuleCreator from '../../ContentCreator/LessonModuleCreator/LessonModuleCreator';
+import UnitCreator from '../../ContentCreator/UnitCreator/UnitCreator';
+import UnitEditor from '../../ContentCreator/UnitEditor/UnitEditor';
 
 const { TabPane } = Tabs;
 
@@ -16,6 +21,30 @@ export default function Classroom({
   setSelectedActivity,
 }) {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [visible, setVisible] = useState(false)
+  const [grade, setGrade] = useState("")
+  const [name, setName] = useState("")
+  const [number, setNumber] = useState("")
+  const [description, setDescription] = useState("")
+  const [standard, setStandard] = useState("")
+  const [gradeList, setGradeList] = useState([])
+  const [learningStandardList, setLessonModuleList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const [lsResponse, gradeResponse] = await Promise.all([
+        getLessonModuleAll(),
+        getGrades(),
+      ]);
+      setLessonModuleList(lsResponse.data);
+
+      const grades = gradeResponse.data;
+      grades.sort((a, b) => (a.id > b.id ? 1 : -1));
+      setGradeList(grades);
+
+    };
+    fetchData();
+  }, []);
 
   const { id } = useParams();
   const tab = searchParams.get('tab');
@@ -50,6 +79,13 @@ export default function Classroom({
             setSearchParams={setSearchParams}
             classroomId={id}
           />
+        </TabPane>
+        <TabPane tab="Create Unit" key="create-unit">
+          <UnitCreator gradeList={gradeList}></UnitCreator>
+        </TabPane>
+        
+        <TabPane tab="Create Lesson" key="create-lesson">
+            <LessonModuleCreator setLessonModuleList={learningStandardList}></LessonModuleCreator>
         </TabPane>
       </Tabs>
     </div>
