9789c41238b2e4f71cee695d9c3ae2b3955df5cb
annikajc <ajcruz146@gmail.com>
2023-11-28 16:11:23 -0500

    Add checkboxes with classroom names to ClassroomsModal
    Meant to add students to other classrooms; not functional

--- client/src/views/Mentor/Classroom/Roster/ClassroomsModal.jsx
+++ client/src/views/Mentor/Classroom/Roster/ClassroomsModal.jsx
@@ -1,8 +1,33 @@
 import { Modal, Button, Checkbox } from 'antd';
-import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { getMentor, getClassrooms } from '../../../../Utils/requests';
+import { message } from 'antd';
+import { useGlobalState } from '../../../../Utils/userState';
 
 export default function ClassroomsModal({ linkBtn, student }) {
   const [visible, setVisible] = useState(false);
+  const [selectedClassrooms, setSelectedClassrooms] = useState([]);
+  const [classrooms, setClassrooms] = useState([]);
+  const [value] = useGlobalState('currUser');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let classroomIds = [];
+    getMentor().then((res) => {
+      if (res.data) {
+        res.data.classrooms.forEach((classroom) => {
+          classroomIds.push(classroom.id);
+        });
+        getClassrooms(classroomIds).then((classrooms) => {
+          setClassrooms(classrooms);
+        });
+      } else {
+        message.error(res.err);
+        navigate('/teacherlogin');
+      }
+    });
+  }, []);
 
   const showModal = () => {
     setVisible(true);
@@ -37,9 +62,31 @@ export default function ClassroomsModal({ linkBtn, student }) {
         <div id='modal-card-content-container'>
           <div id='description-container'>
             <p id='label'>Classrooms</p>
-            <p id='label-info'>
+            {classrooms.map((classroom) => (
+              <Checkbox
+                key={classroom.id}
+                style={{ display: 'block' }}
+                onChange={(e) => {
+                  if (e.target.checked) {
+                    setSelectedClassrooms([
+                      ...selectedClassrooms,
+                      classroom.id,
+                    ]);
+                  } else {
+                    setSelectedClassrooms(
+                      selectedClassrooms.filter(
+                        (id) => id !== classroom.id
+                      )
+                    );
+                  }
+                }}
+              >
+                {classroom.name}
+              </Checkbox>
+            ))}
+            {/* <p id='label-info'>
               {student.enrolled.enrolled ? 'Enrolled' : 'Unenrolled'}
-            </p>
+            </p> */}
           </div>
         </div>
       </Modal>
